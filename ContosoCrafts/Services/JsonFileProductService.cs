b5f0195a3f3231d3df86fba946ac7e8e4ea970ca
Zahra <zbehfarshad@gmail.com>
2020-06-01 17:42:42 -0700

    Contoso Crafts is a learning ASP.NET Core project

---
+++ ContosoCrafts/Services/JsonFileProductService.cs
@@ -0,0 +1,38 @@
+﻿using ContosoCrafts.Models;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.Services
+{
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions{
+                    PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+
+}
