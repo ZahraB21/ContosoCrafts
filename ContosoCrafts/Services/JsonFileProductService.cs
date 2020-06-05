7e4cb358935b410ce69163781a1a33e30207a4e5
Zahra <zbehfarshad@gmail.com>
2020-06-04 23:05:38 -0700

    AddRating allows the user to add rating to the product based on the ID

--- ContosoCrafts/Services/JsonFileProductService.cs
+++ ContosoCrafts/Services/JsonFileProductService.cs
@@ -20,7 +20,7 @@ namespace ContosoCrafts.Services
 
         private string JsonFileName
         {
-            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products"); }
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
         }
 
         public IEnumerable<Product> GetProducts()
@@ -28,10 +28,41 @@ namespace ContosoCrafts.Services
             using (var jsonFileReader = File.OpenText(JsonFileName))
             {
                 return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
-                    new JsonSerializerOptions{
-                    PropertyNameCaseInsensitive = true
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
                     });
             }
+        }
+
+        public void AddRating(int rating, string productId)
+        {
+            var products = GetProducts();
+
+            var query = products.First(x => x.Id == productId);
+
+            if(query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            } 
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using ( var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions{
+                        SkipValidation = true,
+                        Indented = false
+                    }),
+                    products
+                );
+            }
+
         }
     }
 
